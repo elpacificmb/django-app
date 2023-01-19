f7c1c11d57995f883db260fbca38dca4266f19b6
El Pacific Binagha <elpacificmb@gmail.com>
2023-01-19 18:20:36 +0200

    Added an autor with One to many relationship to the Post Model

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Post(models.Model):
-
     # Define status choice for draft or published posts
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -13,6 +13,9 @@ class Post(models.Model):
     # Fields for a Blog Post
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
